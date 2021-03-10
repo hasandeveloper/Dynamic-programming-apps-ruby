6c381cc80c9605d84a7deb40a8f78e1a0616d0d8
hasan <hasan.ali@kloctechnologies.co.in>
2021-03-10 11:30:13 +0530

    feat: file search algo

---
+++ Algorithms/file_search/file_search.rb
@@ -0,0 +1,16 @@
+def search full_file_path, file_name
+     puts "checking in #{full_file_path}"
+    if full_file_path.include? file_name
+        p "found #{file_name} in #{full_file_path}"
+        true
+    elsif File.file? full_file_path
+        false
+    else
+        for f in Dir.children(full_file_path)
+            found = search(File.join(full_file_path, f), file_name)
+            return true if found
+        end
+    end
+end
+
+search("/Users/hasan/Documents/Learnings/DS&ALGO Ruby/Advanced", "depth_or_height_of_b_tree.rb")
