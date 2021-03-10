60f14ac12ecb10fe10a99b3933b84b1aaba37db4
hasan <hasan.ali@kloctechnologies.co.in>
2021-03-10 11:32:05 +0530

    chore: return false if not found

--- Algorithms/file_search/file_search.rb
+++ Algorithms/file_search/file_search.rb
@@ -10,7 +10,8 @@ def search full_file_path, file_name
             found = search(File.join(full_file_path, f), file_name)
             return true if found
         end
+        false
     end
 end
 
-search("/Users/hasan/Documents/Learnings/DS&ALGO Ruby/Advanced", "depth_or_height_of_b_tree.rb")
+search("/Users/hasan/Documents/Learnings/DS&ALGO Ruby/Advanced", "depth_or_height_of_b_tree.b")
